405f2ae372e9f67e58c0f79dc0d0230034e4315d
dni ⚡ <office@dnilabs.com>
2024-09-03 16:38:04 +0200

    bug: use incoming payment for check
    internal invoices werent working

--- views_api.py
+++ views_api.py
@@ -139,7 +139,7 @@ async def api_invoices_check_payment(invoice_id: str, payment_hash: str):
             status_code=HTTPStatus.NOT_FOUND, detail="Invoice does not exist."
         )
     try:
-        payment = await get_standalone_payment(payment_hash)
+        payment = await get_standalone_payment(payment_hash, incoming=True)
         assert payment, "Payment not found."
         status = await payment.check_status()
         return {"paid": status.paid}
